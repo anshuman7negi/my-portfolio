29d14f79356394406bccb3a98a4df95dff1cfe12
anshuman7negi <anshumannegi777@gmail.com>
2023-06-19 14:01:30 +0530

    Added linter.yml file

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('hamburger').addEventListener('click',()=>{
+    console.log("hellow");
+    document.getElementsByClassName('mobile-menu').style.visibility=== visible;
+});
