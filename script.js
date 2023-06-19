c7b48ed4d7be84b920074fed837c32a70b1e6c35
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-19 15:50:03 +0500

    Blur the sections when popup appears

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-console.log("geflakfj")
 document.querySelector('.hamburger').addEventListener('click',()=>{
-    console.log("hellow");
     document.querySelector('.mobile-menu').classList.toggle('visible')
+    const sections = document.querySelectorAll('section, .logo, .hamburger');
+    for (let i = 0; i < sections.length; i += 1) {
+        console.log(sections[i]);
+        sections[i].classList.toggle('blurred');
+    }
 });
