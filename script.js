fb1decbba7186362ae409fde7da054b26e7d0f17
anshuman7negi <anshumannegi777@gmail.com>
2023-06-19 17:06:39 +0530

    Solved linter errors

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-document.querySelector('.hamburger').addEventListener('click',()=>{
-    document.querySelector('.mobile-menu').classList.toggle('visible')
-    const sections = document.querySelectorAll('section, .logo, .hamburger');
-    for (let i = 0; i < sections.length; i += 1) {
-        console.log(sections[i]);
-        sections[i].classList.toggle('blurred');
-    }
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('visible');
+  const sections = document.querySelectorAll('section, .logo, .hamburger');
+  for (let i = 0; i < sections.length; i += 1) {
+    sections[i].classList.toggle('blurred');
+  }
 });
 
 const closeBtn = document.querySelector('.cancel-btn');
 closeBtn.addEventListener('click', () => {
-    document.querySelector('.mobile-menu').classList.toggle('visible');
-    const sections = document.querySelectorAll('section, .logo, .hamburger');
-    for (let i = 0; i < sections.length; i += 1) {
-        sections[i].classList.toggle('blurred');
- }
+  document.querySelector('.mobile-menu').classList.toggle('visible');
+  const sections = document.querySelectorAll('section, .logo, .hamburger');
+  for (let i = 0; i < sections.length; i += 1) {
+    sections[i].classList.toggle('blurred');
+  }
 });
 
 const linkBtn = document.querySelectorAll('.mobile-menu ul a');
@@ -22,7 +21,7 @@ for (let i = 0; i < linkBtn.length; i += 1) {
     document.querySelector('.mobile-menu').classList.toggle('visible');
     const sections = document.querySelectorAll('section, .logo, .hamburger');
     for (let i = 0; i < sections.length; i += 1) {
-        sections[i].classList.toggle('blurred');
- }
+      sections[i].classList.toggle('blurred');
+    }
   });
 }
