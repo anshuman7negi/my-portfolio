31a01bb0a223611903d85e0ae720a932361ddd3d
anshuman7negi <anshumannegi777@gmail.com>
2023-06-20 14:05:50 +0530

    Created object for card data in data.js file

---
+++ data.js
@@ -0,0 +1,30 @@
+const cardData=[
+    {
+        "name":"Tonic",
+        "heading":["CANOPY","Back End Dev","2015"],
+        "cardDetail":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        "skills":["html","css","js"],
+        "image":"images/third-card.svg"
+    },
+    {
+        "name":"Tonic",
+        "heading":["CANOPY","Back End Dev","2015"],
+        "cardDetail":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        "skills":["html","css","js"],
+        "image":"images/third-card.svg"
+    },
+    {
+        "name":"Tonic",
+        "heading":["CANOPY","Back End Dev","2015"],
+        "cardDetail":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        "skills":["html","css","js"],
+        "image":"images/third-card.svg"
+    },
+    {
+        "name":"Tonic",
+        "heading":["CANOPY","Back End Dev","2015"],
+        "cardDetail":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        "skills":["html","css","js"],
+        "image":"images/third-card.svg"
+    }
+];
