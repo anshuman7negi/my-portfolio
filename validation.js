d05ee2e565d6f264dca5646fbfe49f24a4eff5fc
anshuman7negi <anshumannegi777@gmail.com>
2023-06-21 16:19:49 +0530

    Soved linter errors

--- validation.js
+++ validation.js
@@ -1,36 +1,31 @@
 function showMessage(input, message, type) {
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	input.className = type ? "success" : "error";
-	return type;
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  input.className = type ? 'success' : 'error';
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
-}
-
-function showSuccess(input) {
-	return showMessage(input, "", true);
+  return showMessage(input, message, false);
 }
 
 function validateEmail(input, invalidMsg) {
-	const emailRegex =/^[^A-Z\s]+\.[^A-Z\s]+$/;
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
+  const emailRegex = /^[^A-Z\s]+\.[^A-Z\s]+$/;
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
 }
 
-const form = document.querySelector("#contact-page fieldset");
-const submissionForm = document.querySelector("#contact-page form");
-const EMAIL_INVALID = "Please enter email in lower case !";
+const form = document.querySelector('#contact-page fieldset');
+const submissionForm = document.querySelector('#contact-page form');
+const EMAIL_INVALID = 'Please enter email in lower case !';
 
-submissionForm.addEventListener("submit", function (event) {
+submissionForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  let emailValid = validateEmail(form.elements["useremail"], EMAIL_INVALID);
+  const emailValid = validateEmail(form.elements.useremail, EMAIL_INVALID);
   if (emailValid) {
     form.submit();
   }
 });
-
