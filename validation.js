589dd3d2ef99f58a1f8922ae0de37058d34615b1
anshuman7negi <anshumannegi777@gmail.com>
2023-06-21 14:59:22 +0530

    Added function to display message

---
+++ validation.js
@@ -0,0 +1,19 @@
+function showMessage(input, message, type) {
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+
+
+
+
