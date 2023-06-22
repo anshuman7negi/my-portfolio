078d9415b24ed0ea6fcda6e2bdf825d99b4c8ea9
anshuman7negi <anshumannegi777@gmail.com>
2023-06-22 17:36:27 +0530

    Added local storage to contact fomr

--- validation.js
+++ validation.js
@@ -22,6 +22,27 @@ const form = document.querySelector('#contact-page fieldset');
 const submissionForm = document.querySelector('#contact-page form');
 const EMAIL_INVALID = 'Please enter email in lower case !';
 
+function storeInputData() {
+  const inputData = {};
+  for (let i = 0; i < form.elements.length; i += 1) {
+    const element = form.elements[i];
+    if (element.value !== '') inputData[element.name] = element.value;
+  }
+  localStorage.setItem('formData', JSON.stringify(inputData));
+}
+
+function retriveInputData() {
+  const recieveData = localStorage.getItem('formData');
+  if (recieveData) {
+    const InputData = JSON.parse(recieveData);
+    for (let i = 0; i < form.elements.length; i += 1) {
+      const element = form.elements[i];
+      element.value = InputData[element.name];
+    }
+  }
+}
+retriveInputData();
+
 submissionForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const emailValid = validateEmail(form.elements.useremail, EMAIL_INVALID);
@@ -29,6 +50,7 @@ submissionForm.addEventListener('submit', (event) => {
     const nameInput = form.querySelector('input[name="username"]');
     const emailInput = form.querySelector('input[name="useremail"]');
     const messageInput = form.querySelector('textarea[name="message"]');
+    storeInputData();
     nameInput.value = ' ';
     emailInput.value = ' ';
     messageInput.value = ' ';
