f0a2ab52296470f37ed1e968e62ea329e3269fe2
anshuman7negi <anshumannegi777@gmail.com>
2023-06-22 14:20:50 +0530

    Value removed after click on subit button

--- validation.js
+++ validation.js
@@ -26,6 +26,12 @@ submissionForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const emailValid = validateEmail(form.elements.useremail, EMAIL_INVALID);
   if (emailValid) {
+    const nameInput = form.querySelector('input[name="username"]');
+    const emailInput = form.querySelector('input[name="useremail"]');
+    const messageInput = form.querySelector('textarea[name="message"]');
+    nameInput.value = ' ';
+    emailInput.value = ' ';
+    messageInput.value = ' ';
     submissionForm.submit();
   }
 });
